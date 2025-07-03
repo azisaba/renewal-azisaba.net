9ae6f45875100e0771069235dbc45f0f94497023
acrylic-style <me@acrylicstyle.xyz>
2025-07-03 20:59:25 +0900

    Fix color mode

--- components/ToggleColorMode.vue
+++ components/ToggleColorMode.vue
@@ -1,29 +1,24 @@
 <template>
   <div style="display: flex; align-items: center;">
     <button @click="toggleColorMode" style="border: none; background: none; cursor: pointer;">
-      <span v-if="colorMode.value === 'dark'" style="font-size: 24px;"><i class="bi bi-moon"></i></span>
+      <span v-if="currentMode && currentMode === 'dark'" style="font-size: 24px;"><i class="bi bi-moon"></i></span>
       <span v-else style="font-size: 24px;"><i class="bi bi-sun-fill"></i></span>
     </button>
   </div>
 </template>
 
-<script setup>
+<script lang="ts" setup>
+import {ref} from "vue";
+
 const colorMode = useColorMode();
-colorMode.preference = 'system';
+
+// TODO: If delayed init can be removed, it would be very great
+let currentMode = ref<string>();
+onMounted(() => { currentMode.value = colorMode.value });
 
 const toggleColorMode = () => {
-  colorMode.preference = (()=>{
-    console.log(colorMode.value, colorMode.preference);
-    switch(colorMode.value) {
-      case 'dark':
-        return 'light';
-      case 'light':
-        return 'dark';
-      default:
-        return 'system';
-    }
-  })()
-  console.log("pr", colorMode.preference);
+  colorMode.preference = colorMode.value === 'light' ? 'dark' : 'light'
+  currentMode.value = colorMode.value;
 };
 </script>
 
