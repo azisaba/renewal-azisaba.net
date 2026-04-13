a1d2b4ab3d4071a999d051a79eb98cf608b5f5ed
huda0209 <huda030209@gmail.com>
2026-04-13 20:00:04 +0900

    feat: wrap the ArticleCard in a per-page component

---
+++ components/ArticleCard/NewsArticleCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <ArticleCard :path="path" :image="image" :image_cover="image_cover">
+        <template #title>
+            {{ title }}
+        </template>
+        <template #description>
+            {{ description }}
+        </template>
+        <template #meta>
+            {{ date && ` on ${dayjs(date).format("YYYY-MM-DD")}` }}
+        </template>
+    </ArticleCard>
+</template>
+
+<script lang="ts" setup>
+import dayjs from 'dayjs'
+dayjs().format()
+
+defineProps<{
+    path: string,
+    title: string,
+    description: string | null,
+    date: Date | null,
+    image: string | null,
+    image_cover: boolean | undefined,
+}>()
+</script>
+
+<style scoped></style>
---
+++ components/ArticleCard/RecruitArticleCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <ArticleCard :path="path" :image="image" :image_cover="image_cover">
+        <template #title>
+            {{ title }}
+        </template>
+        <template #description>
+            {{ description }}
+        </template>
+        <template #meta>
+            <slot name="tags" />
+        </template>
+    </ArticleCard>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+    path: string,
+    title: string,
+    description: string | null,
+    image: string | null,
+    image_cover: boolean | undefined,
+}>()
+</script>
+
+<style scoped></style>
