96fc327930e208693219b78950d7bfeb1e35d12e
huda0209 <huda030209@gmail.com>
2026-04-16 02:13:26 +0900

    feat: 新メニューを適用

--- layouts/markdown.vue
+++ layouts/markdown.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="page">
-    <NavigationBar />
+    <Header />
     <div class="content">
       <slot />
     </div>
@@ -16,7 +16,7 @@
 }
 
 .content {
-  flex: 1; /* flexに1つだけ数値を指定するとその要素は伸びる */
+    flex: 1; /* flexに1つだけ数値を指定するとその要素は伸びる */
 }
 
 a {
