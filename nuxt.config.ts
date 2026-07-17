361697dbcfe831d7aecf95ec9cad616ec7dd9c6e
tksimeji <tksimeji@outlook.com>
2026-07-18 08:16:07 +0900

    commit for now

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -81,9 +81,6 @@ export default defineNuxtConfig({
     },
   },
   routeRules: {
-    "/": { prerender: false },
-    "/patch-notes": { prerender: false },
-    "/patch-notes/**": { prerender: false },
     "/terms": { redirect: "/rules/terms" },
     "/minecraft-server-policy": { redirect: "/rules/minecraft-server-policy" },
     "/minecraft-all-srv-rule": { redirect: "/rules/global-server-rules" },
